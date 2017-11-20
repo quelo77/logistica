5e8fcc4d7dce626f9c1b77d9a1bc9a4abab8a501
elandy2013 <elandy_2009@yahoo.com.ar>
2017-11-19 23:46:18 -0300

    sigo modificando el head.php

--- source/inc/head.php
+++ source/inc/head.php
@@ -3,15 +3,13 @@
     header('Access-Control-Allow-Origin: *');
 ?>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
-    <title>Dirty Trucks Inc.</title>
-    <link rel="shortcut icon" href="favicon.ico" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-    <!--<link href='https://fonts.googleapis.com/css?family=Cabin:400,700' rel='stylesheet'/>-->
-
-<!--    <link rel="stylesheet" href="assets/css/vendor/materialize.css"/>-->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
-    <link rel="stylesheet" href="assets/css/vendor/sweetalert2.css"/>
-    <link rel="stylesheet" href="assets/css/main.css"/>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Dirty Trucks Inc.</title>
+	<link rel="shortcut icon" href="favicon.ico">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--<link rel="stylesheet" href="assets/css/vendor/materialize.css"/>-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/vendor/sweetalert2.css">
+	<link rel="stylesheet" href="assets/css/main.css">
 </head>
