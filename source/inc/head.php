5df1cf76f0dea8ed34b7960bfb20b050beaed5c7
elandy2013 <elandy_2009@yahoo.com.ar>
2017-11-19 23:18:11 -0300

    cambios en el head.php

--- source/inc/head.php
+++ source/inc/head.php
@@ -4,14 +4,14 @@
 ?>
 <head>
     <meta charset="UTF-8">
-    <title>Dirty Trucks Inc.</title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
+    <title>Dirty Trucks Inc.</title>
     <link rel="shortcut icon" href="favicon.ico" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Cabin:400,700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+    <link href='https://fonts.googleapis.com/css?family=Cabin:400,700' rel='stylesheet'/>
 
 <!--    <link rel="stylesheet" href="assets/css/vendor/materialize.css"/>-->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/vendor/sweetalert2.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="assets/css/vendor/sweetalert2.css"/>
     <link rel="stylesheet" href="assets/css/main.css"/>
 </head>
