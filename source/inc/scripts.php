a42369724afa91578f99043bff5847997b6e5dbf
elandy2013 <elandy_2009@yahoo.com.ar>
2017-11-29 17:51:39 -0300

    chau materialize!

--- source/inc/scripts.php
+++ source/inc/scripts.php
@@ -1,7 +1,7 @@
 <script type="text/javascript" src="assets/scripts/vendor/jquery-2.1.4.min.js"></script>
 <script type="text/javascript" src="assets/scripts/vendor/googlemapsAPI.js"></script>
 <script type="text/javascript" src="assets/scripts/vendor/gmaps.js"></script>
-<script type="text/javascript" src="assets/scripts/vendor/materialize.js"></script>
+<!--<script type="text/javascript" src="assets/scripts/vendor/materialize.js"></script>-->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="assets/scripts/vendor/sweetalert2.min.js"></script>
 <!--<script type="text/javascript" src="assets/scripts/ComponentesMaterialize.js"></script>-->
